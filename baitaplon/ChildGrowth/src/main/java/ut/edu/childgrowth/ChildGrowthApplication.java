f1b65669debfeb2b774cf1c63c7ba9aa3277fb0c
duynam2802 <duynam.28.02.03@gmail.com>
2025-03-27 17:03:41 +0700

    add java project

---
+++ baitaplon/ChildGrowth/src/main/java/ut/edu/childgrowth/ChildGrowthApplication.java
@@ -0,0 +1,13 @@
+package ut.edu.childgrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChildGrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChildGrowthApplication.class, args);
+    }
+
+}
---
+++ baitaplon/ChildGrowth/src/test/java/ut/edu/childgrowth/ChildGrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package ut.edu.childgrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChildGrowthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
