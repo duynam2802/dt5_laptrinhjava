f0765cf51f9012dc6a9e0a2cc88a71b0d9c6f91d
haonguyen3001 <haonv1380@ut.edu.vn>
2025-04-03 10:34:37 +0700

    sua GrowthChart

--- baitaplon/ChildGrowth/src/main/java/ut/edu/childgrowth/models/GrowthChart.java
+++ baitaplon/ChildGrowth/src/main/java/ut/edu/childgrowth/models/GrowthChart.java
@@ -1,10 +1,10 @@
 package ut.edu.childgrowth.models;
 
 import jakarta.persistence.*;
+import lombok.Data;
 import java.util.Map;
 
-//BIỂU ĐỒ TĂNG TRƯỞNG
-
+@Data
 @Entity
 @Table(name = "growth_charts")
 public class GrowthChart {
@@ -13,56 +13,18 @@ public class GrowthChart {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true)
-    private Long childId;
+    @ManyToOne
+    @JoinColumn(name = "child_id", nullable = false)
+    private Child child;
 
     @ElementCollection
-    @CollectionTable(name = "growth_data_points", joinColumns = @JoinColumn(name = "growthChartId"))
-    @MapKeyColumn(name = "dataType")
+    @CollectionTable(name = "growth_data_points", joinColumns = @JoinColumn(name = "growth_chart_id"))
+    @MapKeyEnumerated(EnumType.STRING)
+    @MapKeyColumn(name = "data_type")
     @Column(name = "value")
-    private Map<String, Double> dataPoints;
+    private Map<GrowthMetric, Double> dataPoints;
 
-    public GrowthChart() {
+    public enum GrowthMetric {
+        WEIGHT, HEIGHT, BMI
     }
-
-    public GrowthChart(Long childId, Map<String, Double> dataPoints) {
-        this.childId = childId;
-        this.dataPoints = dataPoints;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getChildId() {
-        return childId;
-    }
-
-    public void setChildId(Long childId) {
-        this.childId = childId;
-    }
-
-    public Map<String, Double> getDataPoints() {
-        return dataPoints;
-    }
-
-    public void setDataPoints(Map<String, Double> dataPoints) {
-        this.dataPoints = dataPoints;
-    }
-    //Các logic
-//    public String generateChart(String type) {
-//        if (dataPoints == null || !dataPoints.containsKey(type)) {
-//            return "Không có dữ liệu cho " + type;
-//        }
-//        return "Biểu đồ được tạo cho " + type + " với dữ liệu: " + dataPoints.get(type);
-//    }
-//
-//    public void shareChart(Long doctorId) {
-//        System.out.println("Biểu đồ đã được chia sẻ với bác sĩ có ID: " + doctorId);
-//    }
-
 }
